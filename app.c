2729a562b30ccf1afc3efa5566027802154cf725
Mark Orchard-Webb <Mark.Orchard-Webb@silabs.com>
2020-03-27 16:49:30 -0500

    Working copy

--- app.c
+++ app.c
@@ -25,17 +25,27 @@
 #include "gatt_db.h"
 
 #include "app.h"
+//#include "dump.h"
+#ifdef DUMP
+#  undef gecko_cmd_system_set_tx_power
+#endif
+
 #include "common.h"
 #include "struct.h"
+#include <stdio.h>
+#include "em_device.h"
+#include "em_rmu.h"
 
 uint16 delay;
 uint8 adData[31];
+uint8 notify = 0; /* perform notifications of power settings */
 
 struct manData {
 	uint16 id;
 } manData = { .id = 0xffff, };
 
 struct read_data read_data;
+struct powers power;
 
 void set_power(void) {
 	struct gecko_msg_system_set_tx_power_rsp_t *resp;
@@ -55,18 +65,36 @@ void set_ad_data(void) {
 	gecko_cmd_le_gap_bt5_set_adv_data(0,0,read_data.adLen,adData);
 }
 
+int16 req, set, prev;
+uint8 pending = 0, conn;
+
 /* Main application */
 void appMain(gecko_configuration_t *pconfig)
 {
+  read_data.reason = RMU_ResetCauseGet();
+  RMU_ResetCauseClear();
+  pconfig->pa.pa_mode = 0;
   gecko_init(pconfig);
   while (1) {
     struct gecko_cmd_packet* evt;
-    evt = gecko_wait_event();
+	evt = gecko_wait_event();
+#ifdef DUMP
+    dump_event(evt);
+#endif
     switch (BGLIB_MSG_ID(evt->header)) {
       case gecko_evt_system_boot_id:
     	  read_data.reqTxPower = 0;
     	  read_data.interval = 160;
-    	  read_data.adLen = 0;
+    	  read_data.adLen = 10;
+    	  delay = 0;
+    	  power.count = 0;
+    	  for(int16 req = -300; req < 300; req++) {
+    		  power.values[power.count] = gecko_cmd_system_set_tx_power(req)->set_power;
+    		  if(!power.count ||(power.values[power.count] != power.values[power.count-1])) {
+    			  //printf("power: %d\n",power.values[power.count]);
+    			  power.count++;
+    		  }
+    	  }
     	  /* no break */
       case gecko_evt_le_connection_closed_id:
     	  set_power();
@@ -84,6 +112,7 @@ void appMain(gecko_configuration_t *pconfig)
       case gecko_evt_le_connection_opened_id: /***************************************************************** le_connection_opened **/
 #define ED evt->data.evt_le_connection_opened
     	  delay = 0;
+    	  conn = ED.connection;
         break;
 #undef ED
 
@@ -120,18 +149,63 @@ void appMain(gecko_configuration_t *pconfig)
 
       case gecko_evt_gatt_server_user_read_request_id: /*********************************************** gatt_server_user_read_request **/
 #define ED evt->data.evt_gatt_server_user_read_request
-    	  if(gattdb_read == ED.characteristic) {
+    	  switch(ED.characteristic) {
+    	  case gattdb_read:
     		  for(int port = gpioPortA; port <= gpioPortD; port++) {
     			  read_data.gpio[port].model = GPIO->P[port].MODEL;
     			  read_data.gpio[port].modeh = GPIO->P[port].MODEH;
     			  read_data.gpio[port].dout = GPIO->P[port].DOUT;
     		  }
     		  gecko_cmd_gatt_server_send_user_read_response(ED.connection,gattdb_gpio,0,sizeof(read_data),(uint8*)&read_data);
-    	  } else {
+    		  break;
+    	  case gattdb_power:
+      		  gecko_cmd_gatt_server_send_user_read_response(ED.connection,gattdb_power,0,2*power.count,(uint8*)&power.values[0]);
+      		  break;
+    	  default:
     	        gecko_cmd_gatt_server_send_user_read_response(ED.connection,ED.characteristic,1,0,0);
     	  }
         break;
 #undef ED
+
+      case gecko_evt_gatt_server_characteristic_status_id: /*************************************** gatt_server_characteristic_status **/
+#define ED evt->data.evt_gatt_server_characteristic_status
+    	  if(ED.characteristic == gattdb_notify) {
+    		  if(1 == ED.status_flags) {
+    			  if(2 == ED.client_config_flags)
+    	    		  notify = 1;
+    	    		  req = -300;
+    	    		  prev = -301;
+    	    		  gecko_external_signal(1);
+    		  } else if(2 == ED.status_flags) {
+    			  pending = 0;
+	    		  gecko_external_signal(1);
+    		  }
+    	  }
+        break;
+#undef ED
+
+      case gecko_evt_system_external_signal_id: /************************************************************* system_external_signal **/
+    #define ED evt->data.evt_system_external_signal
+    	  if(pending) break;
+		  gecko_cmd_system_halt(1);
+		  while(1) {
+			  if(req > 200) {
+					  break;
+			  }
+			  set = gecko_cmd_system_set_tx_power(req)->set_power;
+			  req++;
+			  if(set != prev) {
+					  prev = set;
+					  pending = 1;
+					  break;
+			  }
+		  }
+		  gecko_cmd_system_halt(0);
+          if(pending) {
+    		  gecko_cmd_gatt_server_send_characteristic_notification(conn,gattdb_notify,2,(uint8*)&set);
+          }
+        break;
+    #undef ED
 
       default:
         break;
--- struct.h
+++ struct.h
@@ -14,9 +14,14 @@ struct gpio {
 
 struct __attribute__((packed)) read_data {
 	struct gpio gpio[4];
-	uint32 interval;
+	uint32 reason, interval;
 	int16 reqTxPower, TxPower;
 	uint8 adLen;
 } read_data;
+
+struct __attribute__((packed)) powers {
+	uint16 count;
+	int16 values[256];
+};
 
 #endif /* STRUCT_H_ */
